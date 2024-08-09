65b42be0f306a384d491fed459db3038ab03b5eb
Ahmed Vally <ahmedvally711@gmail.com>
2024-08-09 11:43:03 +0200

    Add the properties to the Dashboard

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-// Object Types Challenge
-// Based on what we discussed we need to make up our Property Objects and array,
-// can you create that array, making sure to assign the correct Types?
+
+
+const propertyContainer = document.querySelector('.properties')
 
 import { showReviewTotal, populateUser } from './utils'
 let isOpen: boolean
@@ -47,7 +47,7 @@ const you: {
     stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
-//Properties
+// Array of Properties
 const properties : {
     image: string;
     title: string;
@@ -62,7 +62,7 @@ const properties : {
     isAvailable: boolean;
 }[] = [
     {
-        image: '',
+        image: 'images/colombia-property.jpg',
         title: 'Colombian Shack',
         price: 45,
         location: {
@@ -75,7 +75,7 @@ const properties : {
         isAvailable: true  
     },
     {
-        image: '',
+        image: 'images/poland-property.jpg',
         title: 'Polish Cottage',
         price: 34,
         location: {
@@ -88,7 +88,7 @@ const properties : {
         isAvailable: false 
     },
     {
-        image: '',
+        image: 'images/london-property.jpg',
         title: 'London Flat',
         price: 23,
         location: {
@@ -106,4 +106,16 @@ const properties : {
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
 populateUser(you.isReturning, you.firstName)
+
+//Add the properties
+for (let i = 0; i < properties.length; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = properties[i].title
+    const image = document.createElement('img')
+    image.setAttribute('src', properties[i].image)
+    card.appendChild(image)
+    propertyContainer.appendChild(card)
+}
+
 
